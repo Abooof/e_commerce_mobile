dd52d8cee23e3a2553b1de1fd9e850714eb5293b
omar ahmed <omar1772003@gmail.com>
2024-05-26 02:36:13 +0300

    addtocart done

--- lib/providers/AuthProvider.dart
+++ lib/providers/AuthProvider.dart
@@ -9,6 +9,7 @@ class AuthProvider with ChangeNotifier {
   String _userId = "";
   bool _authenticated = false;
   UserModel? _currentUser;
+  String _DBid='';
 
   bool get isAuthenticated {
     return _authenticated;
@@ -25,6 +26,10 @@ class AuthProvider with ChangeNotifier {
 
   String get userId {
     return _userId;
+  }
+
+  String get DBid {
+    return _DBid;
   }
 
   String get role {
@@ -120,16 +125,20 @@ class AuthProvider with ChangeNotifier {
       final responseData = json.decode(response.body) as Map<String, dynamic>;
       print(responseData);
       // Iterate through all users to find the one with the matching email
-      final user = responseData.values.firstWhere(
-        (userData) => userData['email'] == email,
-        orElse: () => null,
-      );
+         final userEntry  = responseData.entries.firstWhere(
+      (entry) => entry.value['email'] == email,
+      orElse: () => MapEntry<String, dynamic>('id', null), // Return a dummy MapEntry if no user is found
+    );
 
-      if (user == null) {
-        throw Exception('User not found');
-      }
+    if (userEntry.value == null) {
+      throw Exception('User not found');
+    }
 
-      return user as Map<String, dynamic>; // Cast user to Map<String, dynamic>
+    // Extract user ID and user data
+    final userId = userEntry.key;
+    final userData = userEntry.value;
+
+    return {'id': userId, 'data': userData}; // Return user ID and user data
     } catch (error) {
       print('Error fetching user by email: $error');
       throw error;
@@ -167,8 +176,12 @@ class AuthProvider with ChangeNotifier {
           seconds: int.parse(authResponseData['expiresIn']),
         ),
       );
-      final userData = await getUserByEmail(email);
-
+      final  userData = await getUserByEmail(email);
+      print(userData);
+      _DBid=(userData['id']) as String;
+      print(_DBid);
+      _currentUser=UserModel.fromJson(userData['data']);
+      print("_currentUser $_currentUser");
       notifyListeners();
 
       return userData;
