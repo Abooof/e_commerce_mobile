663ac9443d8b5a85959ea8c707b056bbf8ccc841
Abooof <Abdullah.abooof@gmail.com>
2024-05-26 06:37:58 +0300

    chore: Refactor code to calculate total price in ProductProvider
    Refactor the `calculateTotalPrice` method in the `ProductProvider` class to correctly calculate the total price of products in the user's cart. Also, add a new method `getProductById` to retrieve a product by its ID.

--- lib/screens/CartScreen.dart
+++ lib/screens/CartScreen.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce_mobile/models/user_model.dart';
+import 'package:e_commerce_mobile/providers/AuthProvider.dart';
+import 'package:e_commerce_mobile/providers/productProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -6,167 +10,133 @@ class CartScreen extends StatefulWidget {
   @override
   _CartScreenState createState() => _CartScreenState();
 }
-
 class _CartScreenState extends State<CartScreen> {
-  List images = [
-    "assets/cart_images/test1.jpg",
-    "assets/cart_images/test2.jpg",
-    "assets/cart_images/test3.jpg",
-    "assets/cart_images/test4.jpg",
-  ];
-
-  List ProductTitles = [
-    "iphone 14",
-    "Product 11",
-    "Product 13",
-    "samsung s6",
-  ];
-
-  List Prices = [
-    "\$1000",
-    "\$1000",
-    "\$1000",
-    "\$2000",
-  ];
-
-  void removeItem(int index) {
-    setState(() {
-      images.removeAt(index);
-      ProductTitles.removeAt(index);
-      Prices.removeAt(index);
-    });
-  }
-
-  double calculateTotal() {
-    return Prices.map((price) => double.parse(price.substring(1))).reduce((a,
-        b) => a + b);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    final productProvider = Provider.of<ProductProvider>(context);
+    final currentUser = authProvider.currentUser;
+    final cartItems = currentUser?.cart ?? [];
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Cart"),
-          leading: const BackButton(),
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.black,
-          elevation: 0,
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: images.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 120,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 1,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3),
+      appBar: AppBar(
+        title: const Text("Cart"),
+        leading: const BackButton(),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                final product = productProvider.getProductById(cartItems[index]);
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 120,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10),
+                            ),
+                            //   image: DecorationImage(
+                            //   image: NetworkImage(product.imageUrl),
+                            //  fit: BoxFit.cover,
                           ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 120,
-                            width: 120,
-                            decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
-                              ),
-                              image: DecorationImage(
-                                image: AssetImage(images[index]),
-                                fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product.name,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
-                          const SizedBox(width: 10),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                ProductTitles[index],
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            const SizedBox(height: 10),
+                            Text(
+                              '\$${product.price.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
                               ),
-                              const SizedBox(height: 10),
-                              Text(
-                                Prices[index],
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          IconButton(
-                            onPressed: () => removeItem(index),
-                            icon: const Icon(Icons.delete),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Text(
-                  "Total: \$${calculateTotal()}",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Checkout",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          onPressed: () {
+                            // productProvider.removeFromCart(product.id, currentUser!);
+                          },
+                          icon: const Icon(Icons.delete),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ),
-            ],
+                );
+              },
+            ),
           ),
-        )
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Total: \$${productProvider.calculateTotalPrice(currentUser!).toStringAsFixed(2)}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Your order is being processed'),
+                ),
+              );
+            },
+            child: const Text(
+              'Checkout',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
